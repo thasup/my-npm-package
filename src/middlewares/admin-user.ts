a38fa076cc52dbe8be6332ab8f6b942af5200e5c
thasup <thanachonfirst@hotmail.com>
2022-02-22 19:54:19 +0700

    Updates

--- src/middlewares/admin-user.ts
+++ src/middlewares/admin-user.ts
@@ -1,22 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { NotAuthorizedError } from "../errors/NotAuthorizedError";
 
-interface UserPayload {
-  id: string;
-  email: string;
-  isAdmin: boolean;
-}
-
-declare global {
-  namespace Express {
-    interface Request {
-      user: UserPayload;
-    }
-  }
-}
-
 export const adminUser = (req: Request, res: Response, next: NextFunction) => {
-  if (req.user && req.user.isAdmin) {
+  if (req.currentUser && req.currentUser.isAdmin) {
     next();
   } else {
     throw new NotAuthorizedError();
