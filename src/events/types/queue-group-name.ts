bbaf9da483063fb4b9b0df0fe1e22e77d1b9f8d1
thasup <thanachonfirst@hotmail.com>
2022-02-26 23:55:04 +0700

    Updates

--- src/events/types/queue-group-name.ts
+++ src/events/types/queue-group-name.ts
@@ -1,5 +1,6 @@
 export enum QueueGroupNames {
   ORDER_SERVICE = "order-service",
   PRODUCT_SERVICE = "product-service",
+  EXPIRATION_SERVICE = "expiration-service",
   PAYMENT_SERVICE = "payment-service",
 }
