c8a014031e7b66c64a8c310fec7e9e5fa7d16439
thasup <thanachonfirst@hotmail.com>
2022-03-09 13:23:35 +0700

    Updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,10 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+interface shippingAddressAttrs {
+  address: string;
+  city: string;
+  postalCode: string;
+  country: string;
+}
+
 interface UserPayload {
   id: string;
   email: string;
   isAdmin: boolean;
+  name: string;
+  image?: string;
+  gender: string;
+  age: number;
+  bio?: string;
+  shippingAddress?: shippingAddressAttrs;
 }
 
 declare global {
